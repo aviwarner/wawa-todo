2ece9ec2954baf599372769b12f1dc0f5455c194
Avi Warner <aviwarner@gmail.com>
2018-07-11 21:02:51 -0700

    Basic auth and api endpoints

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180708210221) do
+ActiveRecord::Schema.define(version: 20180712040145) do
 
   create_table "items", force: :cascade do |t|
     t.string "text"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20180708210221) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
 end
