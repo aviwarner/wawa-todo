530002efa92b14b9fbc8992f0aebe9e14f90a1e0
Avi Warner <aviwarner@gmail.com>
2018-07-13 11:21:01 -0700

    Destroy method for Lists, Users

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -11,8 +11,21 @@ class Api::ListsController < ApiController
     end
   end
 
+  def destroy
+    begin
+      list = List.find(params[:id])
+      list.destroy
+      render json: {}, status: :no_content
+    rescue ActiveRecord::RecordNotFound
+      render json: { "error": "List not found" }, status: :not_found
+    end
+  end
+
   private
   def list_params
-    params.require(:list).permit(:name, :private)
+    params.require(:list).permit(:name, :privatea)
   end
 end
+
+# Destroy test
+# curl -u buymagicfish@gmail.com:asdfjkl -X DELETE http://localhost:3000/api/users/1/lists/1/
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -15,8 +15,21 @@ class Api::UsersController < ApiController
     end
   end
 
+  def destroy
+    begin
+      user = User.find(params[:id])
+      user.destroy
+      render json: {}, status: :no_content
+    rescue ActiveRecord::RecordNotFound
+      render json: { "error": "User not found" }, status: :not_found
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:email, :password)
   end
 end
+
+# Destroy test
+# curl -u buymagicfish@gmail.com:asdfjkl -X DELETE http://localhost:3000/api/users/3/
